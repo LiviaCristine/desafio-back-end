3e81013869d1adbddbe9fb6d4a0471105e82baca
Livia <127121054+LiviaCristine@users.noreply.github.com>
2024-05-05 05:33:58 -0300

    Update pedidos.js

--- pedidos.js
+++ pedidos.js
@@ -1,7 +1,7 @@
 const express = require("express"); // aqui estou iniciando o express 
 const pedidoRoutes = require('./routes/pedidoRoutes'); // Importa o roteador de pedidos
 const userRoutes = require('./routes/userRoutes'); // Importa o roteador de usuários
-const cors = require('cors'); // aqui estou trazendo o pacote cors que permite consumir essa api no fron-end
+const cors = require('cors'); // aqui estou trazendo o pacote cors que permite consumir essa api no front-end
 
 const ConectaBancoDeDados = require('./config/database/bancodeDados'); // aqui estou ligando ao arquivo banco de dados
 ConectaBancoDeDados(); // estou chamando a função que conecta o banco de dados 
